ff693e69665a83b962beb3703bc5819629a667ed
alamin500 <nalamin500@gmail.com>
2021-10-01 09:36:46 +0600

    j

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Header from './components/Header/Header';
 import FriendDetail from './components/FriendDetail/FriendDetail';
 import Culture from './components/Culture/Culture';
+import PostDetail from './components/PostDetail/PostDetail';
 
 function App() {
   return (
@@ -28,6 +29,9 @@ function App() {
           </Route>
           <Route path="/friend/:friendId">
             <FriendDetail></FriendDetail>
+          </Route>
+          <Route path="/post/:postId">
+            <PostDetail></PostDetail>
           </Route>
           <Route exact path="/about">
               <About></About>
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 
 const Post = (props) => {
-    const {title} = props.post;
+    const {title, id} = props.post;
+    const history = useHistory();
+    const handleClick = () => {
+        history.push( `/post/${id}`);
+
+    }
     return (
         <div>
             <h2>{title}</h2>
+            <Link to={ `/post/${id}`}>Post Detail</Link>
+            <br />
+            <button onClick={handleClick}>click to see detail</button>
         </div>
     );
 };
